5c19fad6d997bdfeab0e78adf6eba6a499d05b95
Rogelio Echavarría <90786075+rogelioechavarria@users.noreply.github.com>
2023-03-06 20:01:34 -0600

    New config

---
+++ webservice/config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'dokku-mysql-campusDB';
+$CFG->dbname    = 'campusDB';
+$CFG->dbuser    = 'mysql';
+$CFG->dbpass    = '852668016bf9b32a';
+$CFG->prefix    = 'ucd_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'http://campus.ucarolina.edu.mx';
+$CFG->dataroot  = '/storage';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
