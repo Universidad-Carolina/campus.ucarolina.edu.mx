11730b0e3631c1bd8062b7a0a8bcde6a8cc51dbb
Rogelio Echavarría <90786075+rogelioechavarria@users.noreply.github.com>
2023-03-06 19:23:02 -0600

    Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'moodle/moodle',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '32a58a710f82d6f7bfb7c27d943f85721b5dc2c0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'moodle/moodle' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '32a58a710f82d6f7bfb7c27d943f85721b5dc2c0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
